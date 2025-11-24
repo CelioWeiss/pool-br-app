c3db1ef45817c6fc525f42ce6796f240fd541080
Aguamenti Pool Store <aguamentipoolstore@gmail.com>
2025-11-24 22:44:46 +0000

    Try fixing this error: `Runtime Error: Error: Failed to fetch. Error sou

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,25 +2,14 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   const isConfigured = getApps().length > 0;
   const firebaseApp = isConfigured ? getApp() : initializeApp(firebaseConfig);
-  const auth = getAuth(firebaseApp);
-  const firestore = getFirestore(firebaseApp);
-
-  // This is a special exception to connect to the emulators
-  // during development.
-  if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined' && !auth.emulatorConfig) {
-      const host = window.location.hostname;
-      // These functions are idempotent, so they can be called multiple times.
-      connectAuthEmulator(auth, `http://${host}:9099`, { disableWarnings: true });
-      connectFirestoreEmulator(firestore, host, 8080);
-  }
-
+  
   return getSdks(firebaseApp);
 }
 
