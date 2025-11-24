cea9f5f1abf6b87c85f613bda638c92543f0507e
Aguamenti Pool Store <aguamentipoolstore@gmail.com>
2025-11-24 22:19:27 +0000

    Try fixing this error: `Console Error: FirebaseError: Missing or insuffi

--- src/firebase/non-blocking-updates.tsx
+++ src/firebase/non-blocking-updates.tsx
@@ -22,7 +22,7 @@ export function setDocumentNonBlocking(docRef: DocumentReference, data: any, opt
       'permission-error',
       new FirestorePermissionError({
         path: docRef.path,
-        operation: 'write', // or 'create'/'update' based on options
+        operation: options.merge ? 'update' : 'create',
         requestResourceData: data,
       })
     )
@@ -37,7 +37,7 @@ export function setDocumentNonBlocking(docRef: DocumentReference, data: any, opt
  * Returns the Promise for the new doc ref, but typically not awaited by caller.
  */
 export function addDocumentNonBlocking(colRef: CollectionReference, data: any) {
-  const promise = addDoc(colRef, data)
+  const promise = addDoc(col_ref, data)
     .catch(error => {
       errorEmitter.emit(
         'permission-error',
