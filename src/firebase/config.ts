ffa7b435112d79e1892de6e74e74d30f8b8ce3c2
Aguamenti Pool Store <aguamentipoolstore@gmail.com>
2025-11-24 22:02:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9763385878-e1213",
+  "appId": "1:170338298890:web:d27ed79db7af45c13683f3",
+  "apiKey": "AIzaSyAvnhymms-Mn35WbJFPpM9_2iPi80j52WM",
+  "authDomain": "studio-9763385878-e1213.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "170338298890"
+};
