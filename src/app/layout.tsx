991eb3aaa8758631540a543a19cd16ecdff030bc
Aguamenti Pool Store <aguamentipoolstore@gmail.com>
2025-11-24 21:33:39 +0000

    meu querido, voce fez errado colocou uma imagem que bão e minha log a mi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="https://i.postimg.cc/9f4MNdf7/Design-sem-nome-1-removebg-preview-1.png" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
