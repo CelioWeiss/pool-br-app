fbde7e741ef32350266d99b3db7e50794ce83199
Aguamenti Pool Store <aguamentipoolstore@gmail.com>
2025-12-01 22:16:04 +0000

    preciso que coloque um atalho dentro do menu do tecnico o leitor de qrco

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React from 'react';
@@ -33,7 +34,7 @@ const menuItems = [
   { href: '/dashboard/accounts-receivable', label: 'Contas a Receber', icon: DollarSign, roles: ['owner'] },
   { href: '/dashboard/schedule', label: 'Agenda', icon: Calendar, roles: ['owner', 'technician'] },
   { href: '/dashboard/scanner', label: 'Escanear QR Code', icon: QrCode, roles: ['technician'] },
-  { href: '/dashboard/university', label: 'Universidade', icon: BookOpen, roles: ['master', 'owner'] },
+  { href: '/dashboard/university', label: 'Universidade', icon: BookOpen, roles: ['master', 'owner', 'technician'] },
   // { href: '/dashboard/map', label: 'Mapa', icon: Map, roles: ['master', 'owner'] },
 ];
 
