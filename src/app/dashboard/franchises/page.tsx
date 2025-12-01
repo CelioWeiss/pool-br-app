b984878a9a0e2e5106739626268c99b8138a08f3
Aguamenti Pool Store <aguamentipoolstore@gmail.com>
2025-12-01 22:38:34 +0000

    Try fixing this error: `Build Error: Export useAuth doesn't exist in tar

--- src/app/dashboard/franchises/page.tsx
+++ src/app/dashboard/franchises/page.tsx
@@ -13,7 +13,7 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/alert-dialog';
 import { NewFranchiseForm } from '@/components/dashboard/franchises/new-franchise-form';
 import { useToast } from '@/hooks/use-toast';
-import { useFirestore, useAuth as useFirebaseAuth } from '@/firebase';
+import { useFirestore, useAuthHook as useFirebaseAuth } from '@/firebase';
 import { collection, doc, deleteDoc, writeBatch } from 'firebase/firestore';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useCollection } from '@/firebase';
@@ -227,5 +227,3 @@ export default function FranchisesPage() {
     </>
   );
 }
-
-    
