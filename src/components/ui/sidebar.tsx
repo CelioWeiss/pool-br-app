5e5c795f98601b85ec2409002fdb840e9d61fd02
Aguamenti Pool Store <aguamentipoolstore@gmail.com>
2025-12-02 16:38:06 +0000

    Ele vem diretamente do Tooltip do Radix:
    TooltipContentHoverable.useCal

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -537,7 +537,7 @@ const SidebarMenuButton = React.forwardRef<
   React.ComponentProps<"button"> & {
     asChild?: boolean
     isActive?: boolean
-    tooltip?: string | React.ComponentProps<typeof TooltipContent>
+    tooltip?: string
   } & VariantProps<typeof sidebarMenuButtonVariants>
 >(
   (
@@ -570,12 +570,6 @@ const SidebarMenuButton = React.forwardRef<
       return button
     }
 
-    if (typeof tooltip === "string") {
-      tooltip = {
-        children: tooltip,
-      }
-    }
-
     return (
       <Tooltip>
         <TooltipTrigger asChild>{button}</TooltipTrigger>
@@ -583,8 +577,9 @@ const SidebarMenuButton = React.forwardRef<
           side="right"
           align="center"
           hidden={state !== "collapsed" || isMobile}
-          {...tooltip}
-        />
+        >
+          {tooltip}
+        </TooltipContent>
       </Tooltip>
     )
   }
